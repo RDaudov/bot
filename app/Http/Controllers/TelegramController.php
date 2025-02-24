ea41d7eac0201b3d960acd727a9106a469d24c73
RDaudov <96396811+RDaudov@users.noreply.github.com>
2025-02-25 02:42:22 +0300

    init

---
+++ app/Http/Controllers/TelegramController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class TelegramController extends Controller
+{
+    private $botToken = '7050418756:AAEVnY1AqSt-xQ9xCiLlukd1565rMHib2MU';
+
+    public function handleWebhook(Request $request)
+    {
+        $data = $request->all();
+
+        if (isset($data['message'])) {
+            $chatId = $data['message']['chat']['id'];
+            $text = $data['message']['text'];
+
+            if ($text === '/start') {
+                $this->sendMessage($chatId, 'Привет! Я ваш бот. Используйте /help для списка команд.');
+            } elseif ($text === '/help') {
+                $this->sendMessage($chatId, 'Доступные команды: /start, /help');
+            } else {
+                $this->sendMessage($chatId, 'Я не понимаю эту команду.');
+            }
+        }
+
+        return response('OK', 200);
+    }
+
+    private function sendMessage($chatId, $text)
+    {
+        $url = "https://api.telegram.org/bot" . $this->botToken . "/sendMessage";
+        $data = [
+            'chat_id' => $chatId,
+            'text' => $text,
+        ];
+
+        Http::post($url, $data);
+    }
+}
